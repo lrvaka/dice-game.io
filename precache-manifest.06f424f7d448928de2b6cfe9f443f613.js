ca1b60543b9d5a734777b2c05bfa161216a82365
lukevaka <lukevakasisikakala@gmail.com>
2020-05-07 10:52:48 -0400

    Updates

---
+++ precache-manifest.06f424f7d448928de2b6cfe9f443f613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd6f1d037adc7e93236b898b6e34f80",
+    "url": "/dice-game.io/index.html"
+  },
+  {
+    "revision": "88151bfea4c6f91d6673",
+    "url": "/dice-game.io/static/css/main.00a0f9a3.chunk.css"
+  },
+  {
+    "revision": "8ab761e5d62c9d946f6d",
+    "url": "/dice-game.io/static/js/2.29d94beb.chunk.js"
+  },
+  {
+    "revision": "2881c4a89c3c7ad239c40a9c9582b16d",
+    "url": "/dice-game.io/static/js/2.29d94beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88151bfea4c6f91d6673",
+    "url": "/dice-game.io/static/js/main.257d101e.chunk.js"
+  },
+  {
+    "revision": "c1fbd3890e7f183d76bd",
+    "url": "/dice-game.io/static/js/runtime-main.a103798f.js"
+  },
+  {
+    "revision": "3e7235ce51ad6cbd8739f194393df9ab",
+    "url": "/dice-game.io/static/media/dice.3e7235ce.svg"
+  }
+]);
