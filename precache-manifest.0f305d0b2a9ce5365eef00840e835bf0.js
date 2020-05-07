2b326da3b858cc74fc0f2ab475f35f20a5d0c9c7
lukevaka <lukevakasisikakala@gmail.com>
2020-05-07 18:40:09 -0400

    Updates

---
+++ precache-manifest.0f305d0b2a9ce5365eef00840e835bf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c7087ed7f172caf14c22abf10c3cab",
+    "url": "/dice-game/index.html"
+  },
+  {
+    "revision": "ee5558c2f30632fadf18",
+    "url": "/dice-game/static/css/main.12862d52.chunk.css"
+  },
+  {
+    "revision": "e706f451d45b4d924453",
+    "url": "/dice-game/static/js/2.14999199.chunk.js"
+  },
+  {
+    "revision": "2881c4a89c3c7ad239c40a9c9582b16d",
+    "url": "/dice-game/static/js/2.14999199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5558c2f30632fadf18",
+    "url": "/dice-game/static/js/main.bde94093.chunk.js"
+  },
+  {
+    "revision": "5a2cace92cae4c778f75",
+    "url": "/dice-game/static/js/runtime-main.9e858e00.js"
+  },
+  {
+    "revision": "3dfb0dd26c26964aab9d6752724ef825",
+    "url": "/dice-game/static/media/ThreeGames.3dfb0dd2.png"
+  },
+  {
+    "revision": "3e7235ce51ad6cbd8739f194393df9ab",
+    "url": "/dice-game/static/media/dice.3e7235ce.svg"
+  }
+]);
